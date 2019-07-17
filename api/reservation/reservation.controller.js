b85bfff7099ad993435078f65ce6884c26274ebc
Niv Bekelman <nivb52@gmail.com>
2019-07-17 19:09:43 +0300

    reservation

--- api/reservation/reservation.controller.js
+++ api/reservation/reservation.controller.js
@@ -1,9 +1,20 @@
 const reservationService = require('./reservation.service')
 module.exports = {
     add,
+    getReservations
 }
 
 
+async function getReservations(req, res) {
+    console.log('server reserv controller req.query:', req.query);
+
+    try {
+        const reservations = await reservationService.query(req.query)
+        res.send(reservations)
+    } catch (err) {
+        res.status(500).send({ error: err })
+    }
+}
 async function add(req, res) {
     try {
         const reservation = await reservationService.add(req.body)
