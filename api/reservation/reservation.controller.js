9968a6ef122fc0f1ef6590be82e99331488202a4
Itay Azulay <itay.responder@gmail.com>
2019-07-18 22:04:33 +0300

    asd

--- api/reservation/reservation.controller.js
+++ api/reservation/reservation.controller.js
@@ -5,7 +5,6 @@ module.exports = {
 }
 
 async function getReservations(req, res) {
-    console.log('server reserv controller req.query:', req.query);
     try {
         const reservations = await reservationService.query(req.query)
         res.send(reservations)
@@ -15,7 +14,6 @@ async function getReservations(req, res) {
 }
 
 async function add(req, res) {
-    console.log('server reservationControl add req.body:', req.body)
     try {
         const reservation = await reservationService.add(req.body)
         res.send(reservation)
--- api/reservation/reservation.service.js
+++ api/reservation/reservation.service.js
@@ -13,20 +13,15 @@ async function query(filterBy = {}) {
     }
     const collection = await dbService.getCollection('reservation')
     try {
-        console.log(criteria);
-
         const reservations = await collection.find(criteria).toArray();
-        console.log('server last stop', reservations);
-
         return reservations;
-
     } catch (err) {
         logger.error(`ERROR: cannot get reservations`)
         throw err;
     }
 }
+
 async function add(reservation) {
-    console.log('server lastStop reservation:', reservation)
     const collection = await dbService.getCollection('reservation')
     try {
         await collection.insertOne(reservation)
