c1a45f60529be7fc1823ae1f7f9f9f4c16724384
Itay Azulay <itay.responder@gmail.com>
2019-07-22 17:38:22 +0300

    my first commin heroku

---
+++ public/precache-manifest.69b49ec624150076b3b6ded21d3d545e.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "3835f889a0d6455bd67f89727579c702",
+    "url": "/img/pool.3835f889.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9c30969a7b307c8547a5304495160d7f",
+    "url": "/img/file-document-edit-outline.9c30969a.svg"
+  },
+  {
+    "revision": "40316502d036ad026695",
+    "url": "/css/chunk-vendors.48542c05.css"
+  },
+  {
+    "revision": "a27f35b2c1e5f59cb44d1e61052af1ae",
+    "url": "/img/golf court.a27f35b2.svg"
+  },
+  {
+    "revision": "493600e4fc85752c16126078c2390ad5",
+    "url": "/img/plus-circle-outline.493600e4.svg"
+  },
+  {
+    "revision": "2fc60eb58788225b69fed0ef8bd8ec7a",
+    "url": "/img/no smoking.2fc60eb5.svg"
+  },
+  {
+    "revision": "2532fe3a9cbf4e5e5abe15de76d8e7e9",
+    "url": "/img/information-outline.2532fe3a.svg"
+  },
+  {
+    "revision": "d4c35f746f222c8fbe8f25777d3bacf6",
+    "url": "/img/kitchen.d4c35f74.svg"
+  },
+  {
+    "revision": "84df5662e5066a46284a8e77c17e1f85",
+    "url": "/img/wifi.84df5662.svg"
+  },
+  {
+    "revision": "45b30f6af163a69443f70b9e363a9a6e",
+    "url": "/img/baseline-edit.45b30f6a.svg"
+  },
+  {
+    "revision": "a3257cce82a7cb331dd0a8a2e2eb222a",
+    "url": "/img/email.a3257cce.svg"
+  },
+  {
+    "revision": "ee063532e06a846ee05b41704c9ab55c",
+    "url": "/img/towel.ee063532.svg"
+  },
+  {
+    "revision": "b883cd88b09fb800c8dbc84cde4531d4",
+    "url": "/img/delete-forever-outline.b883cd88.svg"
+  },
+  {
+    "revision": "2cb666a73463f3e70ac62e1190c7108f",
+    "url": "/img/grid.2cb666a7.svg"
+  },
+  {
+    "revision": "d36bd4a7a15f50d1a42ad58049254a48",
+    "url": "/img/baseline-delete.d36bd4a7.svg"
+  },
+  {
+    "revision": "3eceb5cd669e0f8f4e5ce70aa481f2be",
+    "url": "/img/view-list.3eceb5cd.svg"
+  },
+  {
+    "revision": "9165080c3a0097fadba3b7aa7c971829",
+    "url": "/img/jonathan.9165080c.jpg"
+  },
+  {
+    "revision": "0d72d302a9629a8226a0ff591a5f8a1c",
+    "url": "/img/fishing.0d72d302.svg"
+  },
+  {
+    "revision": "aee3477649ea6627dd56",
+    "url": "/js/app.10d967b5.js"
+  },
+  {
+    "revision": "40316502d036ad026695",
+    "url": "/js/chunk-vendors.8fd59375.js"
+  },
+  {
+    "revision": "fd963453f4b04f8d5a9a14f786f8ce32",
+    "url": "/img/bathroom.fd963453.svg"
+  },
+  {
+    "revision": "32d19c01b96dfdf46d1b1f0a582320ff",
+    "url": "/img/close.32d19c01.svg"
+  },
+  {
+    "revision": "7d03c90cd7d10f7ac3b33aee85378a97",
+    "url": "/img/baseline-info.7d03c90c.svg"
+  },
+  {
+    "revision": "3a5aa0462a6004bded4e8e29b3ebcd4c",
+    "url": "/img/smoking allowed.3a5aa046.svg"
+  },
+  {
+    "revision": "2d1702fe7536670f3562967fb6a1f1c5",
+    "url": "/img/lock.2d1702fe.svg"
+  },
+  {
+    "revision": "59d09192312db787603ff56f2047b923",
+    "url": "/img/gym.59d09192.svg"
+  },
+  {
+    "revision": "fa1edcb6634d0120f62fe4ed9e11bac0",
+    "url": "/img/snorkel.fa1edcb6.svg"
+  },
+  {
+    "revision": "cf54fcff796d6882380ce5cae90f6988",
+    "url": "/img/heart-multiple-outline.cf54fcff.svg"
+  },
+  {
+    "revision": "35ef7c5a6bdfafff252e4291d62e02d2",
+    "url": "/img/yacht.35ef7c5a.png"
+  },
+  {
+    "revision": "afd4ace0270f977c04067c1ba59b5c17",
+    "url": "/img/2b11c87.afd4ace0.png"
+  },
+  {
+    "revision": "5e08e6636ef28efa143bb0e79d897bc3",
+    "url": "/img/ac.5e08e663.svg"
+  },
+  {
+    "revision": "a5879ca45f024884edd33bc798af4644",
+    "url": "/img/safe.a5879ca4.svg"
+  },
+  {
+    "revision": "b5fd907dc4b849a6900be1b4c728983e",
+    "url": "/img/heart-multiple.b5fd907d.svg"
+  },
+  {
+    "revision": "33ceb32aad3b94c62a88ef75590c1617",
+    "url": "/img/television-box.33ceb32a.svg"
+  },
+  {
+    "revision": "61dc3de30571ce69fdf1ea7fc7870776",
+    "url": "/img/payment-logos.61dc3de3.png"
+  },
+  {
+    "revision": "71902024c06687d79cf2fe069c810d89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee3477649ea6627dd56",
+    "url": "/css/app.c05670c4.css"
+  }
+];
