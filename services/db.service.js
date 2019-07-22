9b784cf91735eba9d6ca4a0c0424f85995704fcc
Itay Azulay <itay.responder@gmail.com>
2019-07-22 18:17:16 +0300

    asd

--- services/db.service.js
+++ services/db.service.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 // &maxIdleTimeMS= 1000 * 60 * 5 -> 5 min
-const uri = `mongodb+srv://itay:bqgsldL9r0tVYpLJ@cluster0-ipcos.mongodb.net/test?retryWrites=true&w=majority&maxIdleTimeMS=300000`
+const url = `mongodb+srv://itay:bqgsldL9r0tVYpLJ@cluster0-ipcos.mongodb.net/test?retryWrites=true&w=majority&maxIdleTimeMS=300000`
 module.exports = {
     connect,
     getCollection
@@ -9,10 +9,28 @@ module.exports = {
 // Database Name
 const dbName = 'yachts_db';
 var dbConn = null;
-async function connect() {
+function connect() {
+    // Reuse existing connection if exist
+    if (dbConn) return Promise.resolve(dbConn);
+    const MongoClient = require('mongodb').MongoClient;
+    return MongoClient.connect(url, { useNewUrlParser: true })
+        .then(client => {
+            console.log('Connected to MongoDB');
+            // If we get disconnected (e.g. db is down)
+            client.on('close', () => {
+                console.log('MongoDB Diconnected!');
+                client.close()
+                dbConn = null;
+            })
+            dbConn = client.db(dbName)
+            return dbConn;
+        })
+
+}
+async function connect2() {
     if (dbConn) {
         return dbConn;
-    } 
+    }
     try {
         const client = await MongoClient.connect(uri, { useNewUrlParser: true });
         const db = client.db(dbName);
